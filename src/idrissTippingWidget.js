4164c983b7650077322b13b4aed495a537099141
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-22 11:56:12 +0200

    basic tippping popup

--- src/idrissTippingWidget.js
+++ src/idrissTippingWidget.js
@@ -2,6 +2,8 @@ import css from "./tippingStyle.scss";
 import {create} from "fast-creator";
 import {TippingMain} from "./subpages/tippingMain";
 import {TippingAddress} from "./subpages/tippingAddress";
+import {TippingError} from "./subpages/tippingError";
+import {TippingSuccess} from "./subpages/tippingSuccess";
 
 export class IdrissTippingWidget extends HTMLElement {
     constructor() {
@@ -10,11 +12,11 @@ export class IdrissTippingWidget extends HTMLElement {
         this.attachShadow({mode: 'open'})
         this.shadowRoot.append(create('style', {text: css}));
         this.container = create('section.tipping-popup')
-        this.div.shadowRoot.append(this.container);
-        this.process();
+        this.shadowRoot.append(this.container);
+        this.tipProcess();
     }
 
-    async process() {
+    async tipProcess() {
         if (!this.identifier) {
             this.container.append(new TippingAddress().html);
             await new Promise(res => {
@@ -26,7 +28,70 @@ export class IdrissTippingWidget extends HTMLElement {
                 })
             });
         }
-        this.container.append(new TippingMain(this.identifier).html);
+        if (!this.token || !this.tippingValue || !this.network) {
+            this.clearContainer();
+            this.container.append(new TippingMain(this.identifier).html);
+            await new Promise(res => {
+                this.container.addEventListener('sendMoney', e => {
+                    console.log(e);
+                    this.network = e.network;
+                    this.token = e.token;
+                    this.tippingValue = +e.amount;
+                    res()
+                })
+            });
+        }
+        let provider
+        try {
+            provider = await getProvider();
+        } catch (ex) {
+            console.error(ex);
+        }
+        if (!provider) {
+            let urlParams = {
+                identifier: this.identifier,
+                recipient: this.recipient,
+                token: this.token,
+                tippingValue: this.tippingValue,
+                network: this.network
+            }
+            window.open(`https://www.idriss.xyz/tip?` + Object.entries(urlParams).map(([k, v]) => k + '=' + encodeURIComponent(v)).join('&'));
+            return;
+        }
+        this.clearContainer()
+        this.container.append(new TippingWaitingApproval(token).html);
+
+        await TippingLogic.prepareTip(provider, network)
+        this.clearContainer()
+        this.container.append((new TippingWaitingConfirmation(identifier, tippingValue, token)).html)
+        let {
+            integer: amountInteger,
+            normal: amountNormal
+        } = await TippingLogic.calculateAmount(token, tippingValue)
+
+        this.container.querySelector('.amountCoin').textContent = amountNormal;
+        let success = await TippingLogic.sendTip(recipient, amountInteger, network, token, params.get('message') ?? "")
+
+        this.clearContainer()
+        if (success) {
+            let explorerLink;
+            if (this.network == 'ETH')
+                explorerLink = `https://etherscan.io/tx/${success.transactionHash}`
+            else if (this.network == 'BSC')
+                explorerLink = `https://bscscan.com/tx/${success.transactionHash}`
+            else if (this.network == 'Polygon')
+                explorerLink = `https://polygonscan.com/tx/${success.transactionHash}`
+            this.container.append((new TippingSuccess(identifier, explorerLink)).html)
+        } else {
+            this.container.append((new TippingError()).html)
+            console.log({success})
+        }
+    }
+
+    clearContainer() {
+        while (this.container.firstChild) {
+            this.container.firstChild.remove();
+        }
     }
 }
 
