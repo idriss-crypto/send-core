b7bd0486e0754fa65545b97ea6f3c98d89e3d52e
IDriss.xyz <hello@idriss.xyz>
2022-08-24 12:56:48 +0200

    add notifications

--- src/subpages/sendToAnyoneSuccess.js
+++ src/subpages/sendToAnyoneSuccess.js
@@ -6,9 +6,31 @@ import {create} from "fast-creator";
 
 export class SendToAnyoneSuccess {
     constructor(identifier, explorerLink, claimPassword, isIDrissRegistered,
-                assetAmount, assetId, assetType, assetAddress, token, blockNumber) {
+                assetAmount, assetId, assetType, assetAddress, token, blockNumber, txnHash) {
         const idrissHost = IDRISS_HOMEPAGE
         const claimUrl = `${idrissHost}/claim?identifier=${identifier}&claimPassword=${claimPassword}&assetId=${assetId}&assetType=${assetType}&assetAddress=${assetAddress}&token=${token}&blockNumber=${blockNumber}`
+        // ToDo make call to backend to send notification
+        const notificationUrl = `${idrissHost}/sendNotifications`
+        const notificationBody = {
+            url: claimUrl,
+            txnHash: txnHash
+        }
+        const notificationOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            mode: 'cors'
+            body: JSON.stringify(notificationBody),
+        }
+        fetch(notificationUrl, notificationOptions)
+        .then(res => res.json())
+        .then(res=>
+            if (res.status == 200) {
+                this.html.querySelector('#text-wrapper-inner').innerHTML = `We have sent a notification to ${identifier}. Please send them the following link to make sure your gift is arriving:`
+            } else {
+                this.html.querySelector('#text-wrapper-inner').innerHTML = `We could not send a notification to ${identifier}. Please send them the following link to make sure your gift is arriving:`
+            }
+            console.log(res)
+        )
         this.html = create('div', {}, template({identifier, close, success, link, explorerLink, claimUrl}));
         this.html.querySelector('#text-wrapper').style.display = isIDrissRegistered ? 'none' : '';
         this.html.querySelector('.closeButton').onclick = () => this.html.dispatchEvent(Object.assign(new Event('close', {bubbles: true})));
