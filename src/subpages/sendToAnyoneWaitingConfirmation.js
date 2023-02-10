76cc15232915f796c2d149fb1e1d0d0553b0e3b3
lennardevertz <levertz@idriss.xyz>
2023-02-11 00:58:46 +0100

    add custom fee notification for send

--- src/subpages/sendToAnyoneWaitingConfirmation.js
+++ src/subpages/sendToAnyoneWaitingConfirmation.js
@@ -4,23 +4,30 @@ import twitter from "!!url-loader!../img/twitter.svg"
 import {create} from "fast-creator";
 
 export class SendToAnyoneWaitingConfirmation {
-    constructor(identifier, amountUSD, token, amountToken, assetId, assetType, nftName="") {
+    constructor(identifier, isIDrissRegistered, amountUSD, token, amountToken, assetId, assetType, nftName="") {
         this.html = create('div', {}, template({identifier, close, twitter, amountUSD, amountToken, assetId, token, nftName}));
         const subtitleCoin = this.html.querySelector('.subtitleCoin')
         const subtitleNFT = this.html.querySelector('.subtitleNFT')
         const treasuryMessage = this.html.querySelector(".linkContent");
         const twitterIcon = this.html.querySelector(".twitterIcon");
+        const approvalMessage = this.html.querySelector("#numTxns");
         if (identifier.startsWith("@")) twitterIcon.style.display = "block";
         subtitleCoin.style.display = 'none'
         subtitleNFT.style.display = 'none'
+        console.log(isIDrissRegistered)
 
         if (assetType === 'erc721' || assetType === 'erc1155') {
             subtitleNFT.style.display = ''
-            treasuryMessage.innerHTML = "$1 in MATIC supplies IDriss treasury"
+            treasuryMessage.innerHTML = "$1 supplies IDriss' treasury"
+            if (isIDrissRegistered) treasuryMessage.innerHTML = "1% supplies IDriss' treasury"
+            approvalMessage.innerHTML = "Confirm one approval and one transaction in your wallet"
         } else {
             if (assetType === 'erc20') {
-                treasuryMessage.innerHTML = "$1 in MATIC supplies IDriss treasury"
+                treasuryMessage.innerHTML = "$1 supplies IDriss' treasury"
+                if (isIDrissRegistered) treasuryMessage.innerHTML = "1% supplies IDriss' treasury"
+                approvalMessage.innerHTML = "Confirm one approval and one transaction in your wallet"
             }
+            if (isIDrissRegistered) treasuryMessage.innerHTML = "1% supplies IDriss' treasury"
             subtitleCoin.style.display = ''
         }
     }
