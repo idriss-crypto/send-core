2294ead24d96cc3298218d9bdb5a717b6f458509
lennardevertz <levertz@idriss.xyz>
2023-12-20 23:33:02 +0100

    - pricing endpoint

--- src/sendToAnyoneLogic.js
+++ src/sendToAnyoneLogic.js
@@ -136,7 +136,7 @@ export const SendToAnyoneLogic = {
             let response = await (await fetch(`https://api.redstone.finance/prices/?symbol=${redstoneId[ticker]}&provider=redstone&limit=1`)).json();
             priceSt = response[0]["value"]
         } else {
-            let response = await (await fetch(`https://api.portals.fi/v2/tokens?ids=${portal_fi[ticker]}`)).json();
+            let response = await (await fetch(`https://www.idriss.xyz/pricing?token=${portal_fi[ticker]}`)).json();
             priceSt = response['tokens'][0]['price']
         }
 
